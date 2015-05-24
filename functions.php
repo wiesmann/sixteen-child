3f023e2c4f78651768a3d485fdbf0c80647b0bca
Matthias Wiesmann <matthias.wiesmann@gmail.com>
2015-05-24 14:45:47 +0200

    fixed child theme javascript

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function sixteen_child_scripts() {
   if (function_exists('the_curlang')) {
     $timeago_i18n_path = '/js/jquery.timeago.' . the_curlang() . '.js';
     $timeago_i18n_name = 'sixteen-child-timeago-' . the_curlang();
-    wp_enqueue_script($timeago_i18n_name, get_template_directory_uri() . $timeago_i18n_path, array('jquery') );
+    wp_enqueue_script($timeago_i18n_name,  get_stylesheet_directory_uri() . $timeago_i18n_path, array('jquery') );
   }
 }
 
